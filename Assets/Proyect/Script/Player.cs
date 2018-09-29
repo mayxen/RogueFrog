fcc3c18099ca4514dd1e6acb3d9873ca18e721bf
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-09-29 18:43:09 +0100

    segundo
    Añadi el movimiento de la rana y los bounds del mapa

--- Assets/Proyect/Script/Player.cs
+++ Assets/Proyect/Script/Player.cs
@@ -3,17 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+    public float jumpDist;
+    bool jumped;
+    Vector2 screen;
+    Vector3 startingPosition;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+        screen.x = (Screen.width/100f)/2f / Camera.main.aspect;
+        screen.y = (Screen.height/100f)/2f / Camera.main.aspect; 
+        startingPosition = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         float horizontalMov = Input.GetAxis("Horizontal");
         float verticalMov = Input.GetAxis("Vertical");
+        if (!jumped)
+        {
+            if (horizontalMov != 0 )
+            {
+                transform.position = new Vector2(
+                    transform.position.x + (horizontalMov > 0 ? jumpDist : -jumpDist),
+                    transform.position.y
+                    );
+                jumped = true;
+            }
+            if (verticalMov != 0)
+            {
+                
+                transform.position = new Vector2(
+                    transform.position.x ,
+                    transform.position.y + (verticalMov > 0 ? jumpDist : -jumpDist)
+                    );
+                jumped = true;
+            }
+
+           
+        }
+        else
+        {
+            if(horizontalMov==0 && verticalMov == 0)
+            {
+                jumped = false;
+            }
+        }
+
+        //comprobar si se sale de los limites
+
+        if (transform.position.y < -screen.y)
+        {
+            transform.position = new Vector2(transform.position.x,transform.position.y+jumpDist);
+        }else if (transform.position.y > screen.y)
+        {
+            transform.position = startingPosition;
+        }
+        if (transform.position.x < -screen.x)
+        {
+            transform.position = new Vector2(transform.position.x + jumpDist, transform.position.y );
+        }
+        else if (transform.position.x > screen.x)
+        {
+            transform.position = new Vector2(transform.position.x - jumpDist, transform.position.y);
+        }
 
     }
 }
